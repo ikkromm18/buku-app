4cefc3f8c1eda2f9244929202aaaea0e8da5c6c0
ikkromm18 <muhammadikkrom749@gmail.com>
2023-12-28 17:02:39 +0700

    inisiasi proyek laravel, menambahkan tailwind tapi belum bisa

---
+++ resources/views/about.blade.php
@@ -0,0 +1,11 @@
+
+
+@extends('layout.template')
+
+@section('container')
+    <h1>Halalaman About</h1>
+    <div class="bg-blue-500 text-white p-4">
+    Ini adalah elemen dengan latar belakang biru dan teks putih.
+  </div>
+
+@endsection
---
+++ resources/views/home.blade.php
@@ -0,0 +1,5 @@
+@extends('layout.template')
+
+@section('container')
+  <h1>Hello World</h1>
+@endsection
